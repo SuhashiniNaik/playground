6eb4ec21c92a9fad7647f8aa02045c88a989dba7
SuhashiniNaik <suhashini.naik@accenture.com>
2025-09-11 11:56:42 +0000

    Test commit 3
    Change-Id: Idd4fc81b357a58376c5189fe3083ec2432d255c1

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 fn main() {
     println!("Hello, world!");
     println!("Lorem Ipsum.....");
-    println!("Commit test..2");
+    println!("Commit test..3");
     println!("Rim info file should start with {}", dictionary::RIM_INFO);
     println!("Rim info file should start with {}", dictionary::RIM_INFO);
     println!("Rim info file should start with {}", dictionary::RIM_INFO);
